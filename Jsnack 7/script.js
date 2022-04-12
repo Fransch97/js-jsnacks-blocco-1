21167ae2c72d580d5c1037bc297cd3d8dc9aa0bd
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-12 17:22:04 +0200

    JSnack 7

--- Jsnack 7/script.js
+++ Jsnack 7/script.js
@@ -7,3 +7,35 @@ console.log("working");
  perciò per fermarsi prima di 1000 bisogna stampare la potenza prima di incrementarla
  perché altrimenti il controllo viene fatto dopo e si sfora
 */
+const potenza = 2;
+let somma = 2;
+let limit = false;
+const array = [];
+while (!limit){
+    if(somma > 1000){
+        limit = true
+    }else{
+        somma = somma * potenza;
+        console.log(somma);
+        array.push(somma);
+    }
+}
+
+array.pop();
+
+for(i = 0; i<array.length; i++){
+    console.log(array[i]);
+}
+
+
+
+
+
+
+// let cubo = parseInt(prompt("inserisci un numero per il cubo"));
+// const userNumber = cubo
+// for(i = 0; i < 3; i++){
+// cubo = cubo * userNumber;
+// console.log(cubo, i);
+// document.querySelector('body').innerHTML += `<div>${cubo}</div>`
+// }
