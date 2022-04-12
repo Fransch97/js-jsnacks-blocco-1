d1f669a449407a6b59edf788ebc8946d303d2959
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-12 15:47:00 +0200

    Jsnack 1

--- Jsnack 1/script.js
+++ Jsnack 1/script.js
@@ -1,1 +1,44 @@
-console.log("working")
+console.log("working");
+
+/*
+ JSnack 1
+ L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+*/
+alert("In seguito inserisca in ogni richesta un numero");
+let valoreUno = false;
+let valoreDue = false;
+// let valoreUnoInserito = 0;
+
+while(!valoreUno){
+   valoreUnoInserito = parseInt(prompt("Inserire il primo numero"));
+   if(!isNaN(valoreUnoInserito)){
+       valoreUno = true;
+   }
+}
+// let valoreDueInserito = 0;
+while(!valoreDue){
+    valoreDueInserito = parseInt(prompt("Inserire il secondo numero"));
+    if(!isNaN(valoreDueInserito)){
+        valoreDue = true;
+    }
+ }
+
+ 
+
+ if(valoreUnoInserito > valoreDueInserito){
+     
+     document.querySelector('body').innerHTML += `
+    <h1 style="text-align: center;">Il numero maggiore è ${valoreUnoInserito} </h1>
+     
+     `
+ }else if (valoreUnoInserito < valoreDueInserito){
+    document.querySelector('body').innerHTML += `
+   <h1 style="text-align: center;">Il numero maggiore è ${valoreDueInserito} </h1>
+    
+    `
+}else{
+    document.querySelector('body').innerHTML += `
+   <h1 style="text-align: center;">I numeri sono uguali</h1>
+    
+    `
+}
